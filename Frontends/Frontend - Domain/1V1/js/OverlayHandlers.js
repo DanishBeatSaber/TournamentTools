ac23294c057251f17d6e0edfe1b0651ad169bd6d
NightHawk <hasfeldt@yahoo.dk>
2024-04-29 22:42:08 +0200

    Domain-fixes, data-fixes, fuck NPM we yarning now

--- Frontends/Frontend - Domain/1V1/js/OverlayHandlers.js
+++ Frontends/Frontend - Domain/1V1/js/OverlayHandlers.js
@@ -1,7 +1,7 @@
 //Sets the overlay data for Player 1, Player 2 and the round.
 async function setOverlay(playerIDs, playerNames, Round) {
 	document.getElementById("roundTextP").innerText = Round;
-	await fetch('https://spi.danesaber.cf/api/bs/ss/' + playerIDs[0])
+	await fetch('https://spi.hawk.quest/api/bs/ss/' + playerIDs[0])
 		.then(response => response.json())
 		.then(data => {
 			document.getElementById("Player1Name").innerText = playerNames[0];
@@ -9,7 +9,7 @@ async function setOverlay(playerIDs, playerNames, Round) {
 			document.getElementById("Player1Name").style.opacity = '1';
 			document.getElementById("Player1Rank").style.opacity = '0.9';
 		});
-	await fetch('https://spi.danesaber.cf/api/bs/ss/' + playerIDs[1])
+	await fetch('https://spi.hawk.quest/api/bs/ss/' + playerIDs[1])
 		.then(response => response.json())
 		.then(data => {
 			document.getElementById("Player2Name").innerText = playerNames[1];
